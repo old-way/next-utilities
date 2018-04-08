4c3e60e71688e7f6150ea0e842d7733a7d37e3ab
twilroad <269044570@qq.com>
2018-04-08 11:09:21 +0800

    chore(*): add configuration for project open in browser

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+const gulp = require("gulp");
+const rename = require("gulp-rename");
+const sequence = require("gulp-sequence");
+const ts = require("@notadd/gulp-typescript");
+
+const packages = {
+    "open-in-browser": ts.createProject("src/open-in-browser/tsconfig.json"),
+};
+
+const source = "src";
+const distId = process.argv.indexOf("--dist");
+const dist = distId < 0 ? "node_modules/@notadd" : process.argv[distId + 1];
+
+const modules = Object.keys(packages);
+
+gulp.task("default", function () {
+    tasks();
+});
+
+modules.forEach(module => {
+    gulp.task(module, () => {
+        return packages[module]
+            .src()
+            .pipe(packages[module]())
+            .pipe(gulp.dest(`${dist}/${module}`));
+    });
+});
+
+gulp.task("build", function (cb) {
+    sequence("open-in-browser", modules.filter((module) => module !== "open-in-browser"), cb);
+});
+
+function tasks() {
+    modules.forEach(module => {
+        watchTypescript(source, module);
+    });
+}
+
+function watchTypescript(source, module) {
+    gulp.watch(
+        [
+            `${source}/${module}/**/*.ts`,
+            `${source}/${module}/**/*.tsx`,
+            `${source}/${module}/*.ts`,
+            `${source}/${module}/*.tsx`,
+        ],
+        [
+            module,
+        ]
+    ).on("change", function (event) {
+        console.log("File " + event.path + " was " + event.type + ", running tasks...");
+    });
+}
